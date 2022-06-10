e6e2f15a79ac64eae7524dd57409a12a65b5db77
Lin Zhou <lin.zhou@hpi.de>
2022-06-10 14:07:44 +0200

    update mcmc models to match descriptions in the manuscript

--- src/mcmc_models.R
+++ src/mcmc_models.R
@@ -103,6 +103,30 @@ writeLines(modelString_new, con = file.path("likelihood_models", "mixed_model_la
 
 #### model incorporating first order autoregressive (AR1) residual autocorrelation structure
 # following tutorial at: https://agabrioblog.onrender.com/tutorial/autocorrelation-jags/autocorrelation-jags/
+modelString_cauchy_t = "
+model {
+#Likelihood
+for (i in 1:n) {
+mu[i] <- inprod(beta[],X[i,])
+}
+y[1:n] ~ dmnorm(mu[1:n],Omega)
+for (i in 1:n) {
+for (j in 1:n) {
+Sigma[i,j] <- sigma2*(1- phi*phi)*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j))) 
+}
+}
+Omega <- inverse(Sigma)
+
+#Priors
+phi ~ dunif(-1,1)
+for (i in 1:ngroups) {
+beta[i] ~ dnorm(0,1.0E-6)
+}
+tau ~ dscaled.gamma(25, 1)  # half-Cauchy distribution (t distribution with 1.d.f.), scale = 25
+sigma2 = 1/tau
+}
+"
+
 modelString = "
 model {
 #Likelihood
@@ -112,7 +136,7 @@ mu[i] <- inprod(beta[],X[i,])
 y[1:n] ~ dmnorm(mu[1:n],Omega)
 for (i in 1:n) {
 for (j in 1:n) {
-Sigma[i,j] <- sigma2*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j))) 
+Sigma[i,j] <- sigma2*(1- phi*phi)*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j))) 
 }
 }
 Omega <- inverse(Sigma)
@@ -123,20 +147,21 @@ for (i in 1:ngroups) {
 beta[i] ~ dnorm(0,1.0E-6)
 }
 sigma <- z/sqrt(chSq)    # prior for sigma; cauchy = normal/sqrt(chi^2)
-z ~ dnorm(0, 0.04)I(0,)  # half-normal distribution with only positive part
+z ~ dnorm(0, 0.16)I(0,)  # half-normal distribution with only positive part, Cauchy scale = 2.5
 chSq ~ dgamma(0.5, 0.5)  # chi^2 with 1 d.f.
 sigma2 = pow(sigma,2)
 #tau.cor <- tau #* (1- phi*phi)
 }
 "
-modelString_autocor = "
+
+modelString_new = "
 model {
 #Priors
 phi ~ dunif(-1,1)
 for (i in 1:ngroups) {
 beta[i] ~ dnorm(0,1.0E-6)
 }
-z ~ dnorm(0, 0.04)I(0,)  # half-normal distribution with only positive part
+z ~ dnorm(0, 0.16)I(0,)  # half-normal distribution with only positive part, Cauchy scale = 2.5
 chSq ~ dgamma(0.5, 0.5)  # chi^2 with 1 d.f.
 sigma <- z/sqrt(chSq)    # prior for sigma; cauchy = normal/sqrt(chi^2)
 sigma2 = pow(sigma,2)
@@ -155,8 +180,9 @@ Sigma[i,j] <- sigma2*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j)))
 "
 
 # write the model to a text file
+writeLines(modelString_cauchy_t, con = file.path("likelihood_models", "mixed_model_AR1_cauchy_t.txt"))
 writeLines(modelString, con = file.path("likelihood_models", "mixed_model_AR1.txt"))
-writeLines(modelString_autocor, con = file.path("likelihood_models", "new_mixed_model_AR1.txt"))
+writeLines(modelString_new, con = file.path("likelihood_models", "new_mixed_model_AR1.txt"))
 
 #### model incorporating compound symmetry structure
 # following tutorial at: https://agabrioblog.onrender.com/tutorial/autocorrelation-jags/autocorrelation-jags/
