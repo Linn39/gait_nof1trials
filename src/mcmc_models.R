c46c4a42f6bbea58b4925d29ab4c473a84f97dbb
Lin Zhou <lin.zhou@hpi.de>
2022-07-12 11:53:55 +0200

    add AR1 model with informed priors

--- src/mcmc_models.R
+++ src/mcmc_models.R
@@ -22,25 +22,25 @@ model_string = "
 "
 
 model_string_informative_priors = "
-  model {
-  #Likelihood
-  for (i in 1:n) {
-  y[i]~dnorm(mean[i],tau)
-  mean[i] <- inprod(beta[],X[i,])
-  }
-  
-   #Priors
-  beta[1] ~ dnorm(1.36,8.0E-2)
-  for(i in 2:ngroups) {
-  beta[i] ~ dnorm(0,1.0E-3)
-  }
-  sigma ~ dunif(0, 100)
-  tau <- 1 / (sigma * sigma)
-  }
+model {
+#Likelihood
+for (i in 1:n) {
+y[i]~dnorm(mean[i],tau)
+mean[i] <- inprod(beta[],X[i,])
+}
+
+  #Priors
+beta[1] ~ dnorm(1.36,8.0E-2)
+for(i in 2:ngroups) {
+beta[i] ~ dnorm(0,1.0E-3)
+}
+sigma ~ dunif(0, 100)
+tau <- 1 / (sigma * sigma)
+}
 "
 # write the model to a text file
 writeLines(model_string, con = file.path("models", "fact_anovaModel_default.txt")) 
-writeLines(model_string_informative_priors, con = file.path("models", "fact_anovaModel_default_inform.txt")) 
+writeLines(model_string_informative_priors, con = file.path("models", "fact_anovaModel_default_informed.txt")) 
 
 
 #### model incorporating first order autoregressive (AR1) residual autocorrelation structure
@@ -96,6 +96,34 @@ sigma2 = pow(sigma,2)
 }
 "
 
+model_string_informative_priors = "
+model {
+#Likelihood
+for (i in 1:n) {
+mu[i] <- inprod(beta[],X[i,])
+}
+y[1:n] ~ dmnorm(mu[1:n],Omega)
+for (i in 1:n) {
+for (j in 1:n) {
+Sigma[i,j] <- sigma2*(1- phi*phi)*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j))) 
+}
+}
+Omega <- inverse(Sigma)
+
+#Priors
+phi ~ dunif(-1,1)
+beta[1] ~ dnorm(1.36,8.0E-2)
+for(i in 2:ngroups) {
+beta[i] ~ dnorm(0,1.0E-3)
+}
+sigma <- z/sqrt(chSq)    # prior for sigma; cauchy = normal/sqrt(chi^2)
+z ~ dnorm(0, 0.16)I(0,)  # half-normal distribution with only positive part, Cauchy scale = 2.5
+chSq ~ dgamma(0.5, 0.5)  # chi^2 with 1 d.f.
+sigma2 = pow(sigma,2)
+#tau.cor <- tau #* (1- phi*phi)
+}
+"
+
 model_string_new = "
 model {
 #Priors
@@ -124,4 +152,5 @@ Sigma[i,j] <- sigma2*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j)))
 # write the model to a text file
 writeLines(model_string_cauchy_t, con = file.path("models", "mixed_model_AR1_cauchy_t.txt"))
 writeLines(model_string, con = file.path("models", "mixed_model_AR1.txt"))
+writeLines(model_string_informative_priors, con = file.path("models", "mixed_model_AR1_informed.txt"))
 writeLines(model_string_new, con = file.path("models", "new_mixed_model_AR1.txt"))
