4eeaf9434003b91b66f2b1f83d1ccf76b6cf6890
Lin Zhou <lin.zhou@hpi.de>
2022-08-03 14:20:59 +0200

    clean up models for publication

--- src/mcmc_models.R
+++ src/mcmc_models.R
@@ -3,8 +3,8 @@
 #### create folder to save the models
 dir.create(file.path("./models/"), showWarnings = FALSE)
 
-#### define the default likelihood model
-model_string = " 
+#### basic model
+basic_model = " 
   model {
   #Likelihood
   for (i in 1:n) {
@@ -21,7 +21,7 @@ model_string = "
   }
 "
 
-model_string_informative_priors = "
+basic_model_informative_priors_SL = "
 model {
 #Likelihood
 for (i in 1:n) {
@@ -38,38 +38,35 @@ sigma ~ dunif(0, 100)
 tau <- 1 / (sigma * sigma)
 }
 "
-# write the model to a text file
-writeLines(model_string, con = file.path("models", "fact_anovaModel_basic.txt")) 
-writeLines(model_string_informative_priors, con = file.path("models", "fact_anovaModel_basic_informed.txt")) 
 
-
-#### model incorporating first order autoregressive (AR1) residual autocorrelation structure
-# following tutorial at: https://agabrioblog.onrender.com/tutorial/autocorrelation-jags/autocorrelation-jags/
-model_string_cauchy_t = "
+basic_model_informative_priors_ST = "
 model {
 #Likelihood
 for (i in 1:n) {
+y[i]~dnorm(mean[i],tau)
 mean[i] <- inprod(beta[],X[i,])
 }
-y[1:n] ~ dmnorm(mean[1:n],Omega)
-for (i in 1:n) {
-for (j in 1:n) {
-Sigma[i,j] <- sigma2*(1- phi*phi)*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j))) 
-}
-}
-Omega <- inverse(Sigma)
 
-#Priors
-phi ~ dunif(-1,1)
-for (i in 1:ngroups) {
+  #Priors
+beta[1] ~ dnorm(1.05,6.0E-2)
+for(i in 2:ngroups) {
 beta[i] ~ dnorm(0,1.0E-3)
 }
-tau ~ dscaled.gamma(2.5, 1)  # half-Cauchy distribution (t distribution with 1.d.f.), scale = 2.5
-sigma2 = 1/tau
+sigma ~ dunif(0, 100)
+tau <- 1 / (sigma * sigma)
 }
 "
 
-model_string = "
+# write the model to a text file
+writeLines(basic_model, con = file.path("models", "basic.txt")) 
+writeLines(basic_model_informative_priors_SL, con = file.path("models", "basic_informative_SL.txt")) 
+writeLines(basic_model_informative_priors_ST, con = file.path("models", "basic_informative_ST.txt")) 
+
+
+#### model incorporating first order autoregressive (AR1) residual autocorrelation structure
+# refer to tutorial at: https://agabrioblog.onrender.com/tutorial/autocorrelation-jags/autocorrelation-jags/
+
+AR1_model = "
 model {
 #Likelihood
 for (i in 1:n) {
@@ -96,7 +93,7 @@ sigma2 = pow(sigma,2)
 }
 "
 
-model_string_informative_priors = "
+AR1_model_informative_priors_SL = "
 model {
 #Likelihood
 for (i in 1:n) {
@@ -124,33 +121,35 @@ sigma2 = pow(sigma,2)
 }
 "
 
-model_string_new = "
+AR1_model_informative_priors_ST = "
 model {
-#Priors
-phi ~ dunif(-1,1)
-for (i in 1:ngroups) {
-beta[i] ~ dnorm(0,1.0E-3)
-}
-z ~ dnorm(0, 0.16)I(0,)  # half-normal distribution with only positive part, Cauchy scale = 2.5
-chSq ~ dgamma(0.5, 0.5)  # chi^2 with 1 d.f.
-sigma <- z/sqrt(chSq)    # prior for sigma; cauchy = normal/sqrt(chi^2)
-sigma2 = pow(sigma,2)
 #Likelihood
 for (i in 1:n) {
-mean[i] <- inprod(beta[i],X[i,])
+mean[i] <- inprod(beta[],X[i,])
 }
-Omega <- inverse(Sigma)
 y[1:n] ~ dmnorm(mean[1:n],Omega)
 for (i in 1:n) {
 for (j in 1:n) {
-Sigma[i,j] <- sigma2*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j))) 
+Sigma[i,j] <- sigma2*(1- phi*phi)*(equals(i,j) + (1-equals(i,j))*pow(phi,abs(i-j))) 
 }
 }
+Omega <- inverse(Sigma)
+
+#Priors
+phi ~ dunif(-1,1)
+beta[1] ~ dnorm(1.05,6.0E-2)
+for(i in 2:ngroups) {
+beta[i] ~ dnorm(0,1.0E-3)
+}
+sigma <- z/sqrt(chSq)    # prior for sigma; cauchy = normal/sqrt(chi^2)
+z ~ dnorm(0, 0.16)I(0,)  # half-normal distribution with only positive part, Cauchy scale = 2.5
+chSq ~ dgamma(0.5, 0.5)  # chi^2 with 1 d.f.
+sigma2 = pow(sigma,2)
+#tau.cor <- tau #* (1- phi*phi)
 }
 "
 
 # write the model to a text file
-writeLines(model_string_cauchy_t, con = file.path("models", "mixed_model_AR1_cauchy_t.txt"))
-writeLines(model_string, con = file.path("models", "mixed_model_AR1.txt"))
-writeLines(model_string_informative_priors, con = file.path("models", "mixed_model_AR1_informed.txt"))
-writeLines(model_string_new, con = file.path("models", "new_mixed_model_AR1.txt"))
+writeLines(AR1_model, con = file.path("models", "AR1.txt"))
+writeLines(AR1_model_informative_priors_SL, con = file.path("models", "AR1_informative_SL.txt"))
+writeLines(AR1_model_informative_priors_ST, con = file.path("models", "AR1_informative_ST.txt"))
