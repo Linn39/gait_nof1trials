7a0cca513be822ef96db2f76ea18513196fe76e8
Lin Zhou <lin.zhou@hpi.de>
2023-07-14 10:33:18 +0200

    add covariates to ANOVA, format code

--- src/main_statistical_summary.R
+++ src/main_statistical_summary.R
@@ -1,5 +1,5 @@
 #### Statistics summary and ANOVA of the data ####
-rm(list=ls())
+rm(list = ls())
 
 # load libraries and functions
 library(ez)
@@ -7,7 +7,7 @@ source("./src/data_loader.R")
 
 # load data
 downsample_step <- 5
-kw <- 'LFRF_all_strides'
+kw <- "LFRF_all_strides"
 
 # select list of features / gait paarameters
 features_list <- c(
@@ -35,24 +35,27 @@ print("Summary of data from all subjects after downsampling:")
 
 # two-way repeated measures ANOVAA
 results_2_way_anova <- data.frame()
+
 for (var_name in features_list) {
   print_data_summary(loc_df, var_name)
-  
-  dat_df <- select(loc_df, "var" = var_name, sub, condition, fatigue)
-  
-  res.aov <- ezANOVA(
+  colnames(loc_df)[colnames(loc_df) %in% c("height(cm)", "weight(kg)")] <- c("height_cm", "weight_kg") # rename columns
+
+  dat_df <- select(loc_df, "var" = var_name, "sub", "condition", "fatigue", "age", "sex", "height_cm", "weight_kg")
+
+  res_aov <- ezANOVA(
     data = dat_df,
     dv = .(var),
     wid = .(sub),
     within = .(condition, fatigue),
+    within_covariates = .(age, height_cm, weight_kg),
     detailed = T,
     type = 2
   )
-  
-  anova_res <- data.frame(res.aov)
+
+  anova_res <- data.frame(res_aov)
   anova_res$variable <- var_name
   results_2_way_anova <- dplyr::bind_rows(results_2_way_anova, anova_res)
 }
-options(width=1000)  # set the width of the console to display all columns
+options(width = 1000) # set the width of the console to display all columns
 print("ANOVA Summary:")
 print(results_2_way_anova)
