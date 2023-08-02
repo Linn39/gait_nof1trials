f7e80bc0f2e788cee5b4d7fabd405c7cc06fdadf
Lin Zhou <lin.zhou@hpi.de>
2023-08-02 14:15:54 +0200

    test continous correlation and correlation within each level of treatment

--- src/test_LME.R
+++ src/test_LME.R
@@ -1,31 +1,52 @@
+options(warn = -1) # supress warnings from pt function
+
 library(nlme)
 
 #### generate data with AR1 correlation
-sim_ar1_data <- function(rho, n=100){
-#   n <- 100
-  sigma <- 1   # residual standard deviation
-  sigma_e <- 1   # standard deviation of the error term
-  sigma_b <- sigma_e / sqrt(1 - rho^2)
-  sigma_b * arima.sim(n=n, list(ar=rho), sd=sigma)
+sim_ar1_data <- function(rho, n = 100) {
+    sigma <- 1 # residual standard deviation
+    sigma_e <- 1 # standard deviation of the error term
+    sigma_b <- sigma_e / sqrt(1 - rho^2)
+    ar1_err <- sigma_b * arima.sim(n = n, list(ar = rho), sd = sigma)
+    return(ar1_err)
 }
 
-dummy_ar1_data <- function(rho, n=100){
+dummy_ar1_data <- function(rho, n = 100) {
 
 }
 
 # treatment with 2 levels
-trt <- factor(c(rep("A",50), rep("B",50)))
+n_trt <- 50
+trt <- factor(c(rep("A", n_trt), rep("B", n_trt)))
 
 #### test fit of AR1 model
-beta <- rho <- NULL
-for(i in 1:10){
-#   print(i)
-  Y <- rnorm(100) + 2 * as.numeric(trt) + sim_ar1_data(rho=0.5, n=100)
-  fit <- lme(Y ~ trt, random = ~1|trt, correlation = corAR1(form = ~ 1 | trt))  # form = ~ 1 | trt
-  print(summary(fit))
-  print(fit$corStruct)
-  beta[i] <- fit$coefficients$fixed["trtB"]
-  rho[i] <- coef(fit$modelStruct$corStruct)[1]
+true_beta <- 2 # the true treatment effect
+true_rho <- 0.7 # the true AR1 correlation coefficient
+beta <- rho <- ar1_error <- NULL
+for (i in 1:1000) {
+    # continuous correlation
+    ar1_error <- sim_ar1_data(rho = true_rho, n = 2 * n_trt)
+    # correlation within each level of trt
+    ar1_error <- ts(c(sim_ar1_data(rho = true_rho, n = n_trt), sim_ar1_data(rho = true_rho, n = n_trt)))
+
+    # dummy data
+    Y <- rnorm(2 * n_trt) + true_beta * as.numeric(trt) + ar1_error
+
+    # correlation within each level of trt
+    fit <- lme(Y ~ trt, random = ~ 1 | trt, correlation = corAR1(form = ~ 1 | trt))
+    # correlation within all data (doesn't work)
+    # fit <- lme(Y ~ trt, random = ~ 1 | trt, correlation = corAR1(form = ~1))
+
+    # only print in the first loop
+    if (i == 1) {
+        print("summary of the first fit:")
+        print(summary(fit))
+        plot(ar1_error)
+    }
+
+    # collect the estimates
+    beta[i] <- fit$coefficients$fixed["trtB"]
+    rho[i] <- coef(fit$modelStruct$corStruct)[1]
 }
 print("mean beta:")
 print(mean(beta)) # is this close to 2?
