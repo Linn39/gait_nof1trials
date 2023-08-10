f46cba8db785a8c70ab60b03b919b1f11a6bf74b
Lin Zhou <lin.zhou@hpi.de>
2023-08-10 12:49:19 +0200

    add distributoin plots of the estimated parameters

--- src/test_LME.R
+++ src/test_LME.R
@@ -1,6 +1,7 @@
 # options(warn = -1) # supress warnings from pt function
 
 library(nlme)
+library(ggplot2)
 
 #### generate data with AR1 correlation
 sim_ar1_data <- function(rho, n = 100) {
@@ -40,7 +41,7 @@ for (i in 1:1000) {
     # fit <- lme(Y ~ trt, random = ~ 1, correlation = corAR1(form = ~1))
 
     #### gls function (no random effects)
-    fit <- gls(Y ~ trt, correlation = corAR1())     # this works
+    fit <- gls(Y ~ trt, correlation = corAR1()) # this works
     # fit <- gls(Y ~ trt, correlation = corAR1(form = ~ 1 | trt))
 
     # only print in the first loop
@@ -52,13 +53,65 @@ for (i in 1:1000) {
 
     #### collect the estimates
     # both gls and lme should work with this line:
-    rho[i] <- coef(fit$model, unconstrained=FALSE)[["corStruct.Phi"]]
+    rho[i] <- coef(fit$model, unconstrained = FALSE)[["corStruct.Phi"]]
     # for lme model:
-    # beta[i] <- fit$coefficients$fixed["trtB"]  
+    # beta[i] <- fit$coefficients$fixed["trtB"]
     # for gls model:
-    beta[i] <- fit$coefficients[["trtB"]]  
+    beta[i] <- fit$coefficients[["trtB"]]
 }
 print("mean beta:")
 print(mean(beta)) # is this close to 2?
 print("mean rho:")
 print(mean(rho)) # is this close to rho?
+
+#### plot the distribution of beta and rho with density plots, and compare with the true values
+
+# create a data frame with the beta and rho vectors
+df <- data.frame(beta, rho)
+
+options(repr.plot.width = 2, repr.plot.height = 6)
+
+# create a density plot for beta
+beta_plot <- ggplot(df, aes(x = beta)) +
+    geom_density(fill = "blue", alpha = 0.3) +
+    geom_vline(
+        aes(xintercept = mean(beta)),
+        color = "red",
+        linetype = "dashed"
+    ) +
+    ggtitle("Distribution of beta") +
+    xlab("beta") +
+    ylab("Density") +
+    geom_text(
+        aes(
+            x = mean(beta),
+            y = 0.5,
+            label = paste("Mean: ", round(mean(beta), 2))
+        ),
+        color = "red",
+        size = 4
+    )
+
+# create a density plot for rho
+rho_plot <- ggplot(df, aes(x = rho)) +
+    geom_density(fill = "blue", alpha = 0.3) +
+    geom_vline(
+        aes(xintercept = mean(rho)),
+        color = "red",
+        linetype = "dashed"
+    ) +
+    ggtitle("Distribution of rho") +
+    xlab("rho") +
+    ylab("Density") +
+    geom_text(
+        aes(
+            x = mean(rho),
+            y = 0.5,
+            label = paste("Mean: ", round(mean(rho), 2))
+        ),
+        color = "red",
+        size = 4
+    )
+
+# display the plots side by side
+gridExtra::grid.arrange(beta_plot, rho_plot, ncol = 2)
