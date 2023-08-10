52c3717a22b15f5913ef3da9eeac2334fdb69528
Lin Zhou <lin.zhou@hpi.de>
2023-08-10 11:48:26 +0200

    clean up the script, now we can use the gls function to estimate rho, both with and without considering the correlation at the break-up points between conditions. #46

--- src/test_LME.R
+++ src/test_LME.R
@@ -1,4 +1,4 @@
-options(warn = -1) # supress warnings from pt function
+# options(warn = -1) # supress warnings from pt function
 
 library(nlme)
 
@@ -12,14 +12,6 @@ sim_ar1_data <- function(rho, n = 100) {
     return(ar1_err)
 }
 
-dummy_ar1_data <- function(rho, n = 100) {
-    # simulate AR1 error manually
-
-    # du
-    sigma_e <- 1 # standard deviation of the error term
-    cor_ar1 <- corAR1(form = ~1, value = rho)
-}
-
 # treatment with 2 levels
 n_trt <- 50
 trt <- factor(c(rep("A", n_trt), rep("B", n_trt)))
@@ -29,22 +21,27 @@ true_beta <- 2 # the true treatment effect
 true_rho <- 0.7 # the true AR1 correlation coefficient
 beta <- rho <- ar1_error <- NULL
 for (i in 1:1000) {
+    #### generate error with AR1 correlation
     # # continuous correlation
     # ar1_error <- sim_ar1_data(rho = true_rho, n = 2 * n_trt)
     # correlation within each level of trt
     ar1_error <- ts(c(sim_ar1_data(rho = true_rho, n = n_trt), sim_ar1_data(rho = true_rho, n = n_trt)))
 
-    # dummy data
+    #### construct dummy data
+    # with random error
     # Y <- rnorm(2 * n_trt) + true_beta * as.numeric(trt) + ar1_error
+    # without random error
     Y <- true_beta * as.numeric(trt) + ar1_error
 
+    #### lme function (must include random effects)
     # # correlation within each level of trt
     # fit <- lme(Y ~ trt, random = ~ 1 | trt, correlation = corAR1(form = ~ 1 | trt))
     # correlation within all data (doesn't work)
     # fit <- lme(Y ~ trt, random = ~ 1, correlation = corAR1(form = ~1))
 
-    # fit <- gls(Y ~ trt, correlation = corARMA(p = 1))   # this works
-    fit <- gls(Y ~ trt, correlation = corAR1())     # this also works, same as using corARMA(p = 1)
+    #### gls function (no random effects)
+    fit <- gls(Y ~ trt, correlation = corAR1())     # this works
+    # fit <- gls(Y ~ trt, correlation = corAR1(form = ~ 1 | trt))
 
     # only print in the first loop
     if (i == 1) {
@@ -53,12 +50,15 @@ for (i in 1:1000) {
         plot(ar1_error)
     }
 
-    # collect the estimates
-    # beta[i] <- fit$coefficients$fixed["trtB"]  # for lme model
-    rho[i] <- coef(fit$modelStruct$corStruct)[1]
-    beta[i] <- fit$coefficients[["trtB"]]  # for gls model. The same as fit$coefficients$fixed["trtB"]
+    #### collect the estimates
+    # both gls and lme should work with this line:
+    rho[i] <- coef(fit$model, unconstrained=FALSE)[["corStruct.Phi"]]
+    # for lme model:
+    # beta[i] <- fit$coefficients$fixed["trtB"]  
+    # for gls model:
+    beta[i] <- fit$coefficients[["trtB"]]  
 }
 print("mean beta:")
 print(mean(beta)) # is this close to 2?
 print("mean rho:")
-print(mean(rho)) # is this close to RHO?
+print(mean(rho)) # is this close to rho?
